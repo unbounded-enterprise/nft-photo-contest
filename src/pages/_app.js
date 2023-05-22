f332cc1b80f0ab04e2a1dac6b9ec274f9efc1a2f
jackson-laskey <jackson.laskey@gmail.com>
2023-05-22 17:25:53 -0400

    More for social card

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -9,10 +9,13 @@ import { ThemeProvider } from '@mui/material/styles';
 import { AuthConsumer, AuthProvider } from '../contexts/auth-context';
 import { createEmotionCache } from '../utils/create-emotion-cache';
 import { theme } from '../theme';
+import { useRouter } from 'next/router';
+
 
 const clientSideEmotionCache = createEmotionCache();
 
 const App = (props) => {
+  const router = useRouter();
   const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
 
   const getLayout = Component.getLayout ?? ((page) => page);
@@ -28,6 +31,13 @@ const App = (props) => {
           name="viewport"
           content="initial-scale=1, width=device-width"
         />
+        <meta name="twitter:card" content="summary" key="twcard" />
+      <meta property="og:url" content={router.asPath} key="ogurl" />
+      <meta property="og:image" content="https://nftphotocontest.com/static/appImage.png" key="ogimage" />
+      <meta property="og:site_name" content="NFT Photo Contest" key="ogsitename" />
+      <meta property="og:title" content="NFT Photo Contest" key="ogtitle" />
+      <meta property="og:description" content={"See what's happening at the London Blockchain Convention by exploring the NFT Photo Contest and voting for your favorite entries!"} key="ogdesc" />
+   
       </Head>
       <LocalizationProvider dateAdapter={AdapterDateFns}>
         <ThemeProvider theme={theme}>
